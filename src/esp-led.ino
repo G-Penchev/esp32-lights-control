db82b2d2075cf75086690ae5cb459eb6f9406291
G-Penchev <Penchev@live.com>
2023-03-28 00:57:53 +0300

    reformat code

--- src/esp-led.ino
+++ src/esp-led.ino
@@ -1,17 +1,17 @@
+#include <ESPmDNS.h>
+#include <WebServer.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
 
 #include "webpage.h"
 
-//On board LED Connected to GPIO2
+// On board LED Connected to GPIO2
 
-#define LED_GPIO   16
-#define LED_WIFI   23
-#define PWM1_Ch    0
-#define PWM1_Res   8
-#define PWM1_Freq  5000
+#define LED_GPIO 16
+#define LED_WIFI 23
+#define PWM1_Ch 0
+#define PWM1_Res 8
+#define PWM1_Freq 5000
 
 int ledState = 0;
 int maxLedIntensity = 200;
@@ -21,7 +21,7 @@ long previousMillis = 0;
 int inputDebounce;
 int debounceSamples = 0;
 
-//State flags
+// State flags
 bool iterate = false;
 bool turnOnLed = false;
 bool switching = false;
@@ -29,154 +29,153 @@ bool dimmingHigh = false;
 bool touchDetected = false;
 bool ledOn = false;
 
-//SSID and Password of your WiFi router
+// SSID and Password of your WiFi router
 const char* ssid = "cocoapop_deco";
 const char* password = "BruteForce_8991";
 
-//Declare a global object variable from the ESP8266WebServer class.
-WebServer server(80); //Server on port 80
+// Declare a global object variable from the ESP8266WebServer class.
+WebServer server(80);  // Server on port 80
 
 //===============================================================
 // This routine is executed when you open its IP in browser
 //===============================================================
 void handleRoot() {
-  Serial.println("You called root page");
-  String s = MAIN_page; //Read HTML contents
-  server.send(200, "text/html", s); //Send web page
+    Serial.println("You called root page");
+    String s = MAIN_page;              // Read HTML contents
+    server.send(200, "text/html", s);  // Send web page
 }
 
 void handleLEDon() {
-  Serial.println("LED on page");
-  turnOnLed = true;
-  switching = true;
-  server.send(200, "text/html", MAIN_page);
+    Serial.println("LED on page");
+    turnOnLed = true;
+    switching = true;
+    server.send(200, "text/html", MAIN_page);
 }
 
 void handleLEDoff() {
-  Serial.println("LED off page");
-  turnOnLed = false;
-  switching = true;
-  server.send(200, "text/html", MAIN_page);
+    Serial.println("LED off page");
+    turnOnLed = false;
+    switching = true;
+    server.send(200, "text/html", MAIN_page);
 }
 //==============================================================
 //                  SETUP
 //==============================================================
 void setup(void) {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
-  Serial.println("");
+    WiFi.begin(ssid, password);  // Connect to your WiFi router
+    Serial.println("");
 
-  //Onboard LED port Direction output
-  ledcAttachPin(LED_GPIO, PWM1_Ch);
-  ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
+    // Onboard LED port Direction output
+    ledcAttachPin(LED_GPIO, PWM1_Ch);
+    ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
 
-  pinMode(LED_WIFI, OUTPUT);
+    pinMode(LED_WIFI, OUTPUT);
 
-  pinMode(34, INPUT);
-  
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED_WIFI, LOW);
-    delay(150);
-    Serial.print(".");
+    pinMode(34, INPUT);
+
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        digitalWrite(LED_WIFI, LOW);
+        delay(150);
+        Serial.print(".");
+        digitalWrite(LED_WIFI, HIGH);
+        delay(150);
+    }
+
+    // If connection successful show IP address in serial monitor
+    Serial.println("");
     digitalWrite(LED_WIFI, HIGH);
-    delay(150);
-  }
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());  // IP address assigned to your ESP
 
-  //If connection successful show IP address in serial monitor
-  Serial.println("");
-  digitalWrite(LED_WIFI, HIGH);
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+    server.on("/", handleRoot);
+    server.on("/ledOn", handleLEDon);
+    server.on("/ledOff", handleLEDoff);
 
-  server.on("/", handleRoot);  
-  server.on("/ledOn", handleLEDon); 
-  server.on("/ledOff", handleLEDoff);
-
-  server.begin(); 
-  Serial.println("HTTP server started");
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
 bool senseTouch(uint8_t pin) {
+    if (analogRead(pin) < 30) {
+        int touch_debounced = 0;
+        for (int i = 0; i < 5; i++) {
+            delay(2);
+            touch_debounced += analogRead(pin);
+        }
+        touch_debounced = touch_debounced / 5;
 
-  if (analogRead(pin) < 30) {
-    int touch_debounced = 0;
-    for (int i = 0; i < 5; i++) {
-      delay(2);
-      touch_debounced += analogRead(pin);
+        if (touch_debounced < 20) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    touch_debounced = touch_debounced / 5;
-
-    if (touch_debounced < 20) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  return false;
+    return false;
 }
 
 //==============================================================
 //                     LOOPsdad
 //==============================================================
 void loop(void) {
-  server.handleClient();          //Handle client requests
+    server.handleClient();  // Handle client requests
 
-  if (senseTouch(34)) {
-    delay(250);
     if (senseTouch(34)) {
-      //Long press action
-      while (senseTouch(34)) {
-        if (!dimmingHigh && ledState > 0) {
-          ledState -= 1;
-          ledcWrite(PWM1_Ch, ledState);
-          Serial.print("Dimming Low ");
-          Serial.println(ledState);
-        } else if (dimmingHigh && ledState < maxLedIntensity) {
-          ledState += 1;
-          ledcWrite(PWM1_Ch, ledState);
-          Serial.print("Dimming High ");
-          Serial.println(ledState);
+        delay(250);
+        if (senseTouch(34)) {
+            // Long press action
+            while (senseTouch(34)) {
+                if (!dimmingHigh && ledState > 0) {
+                    ledState -= 1;
+                    ledcWrite(PWM1_Ch, ledState);
+                    Serial.print("Dimming Low ");
+                    Serial.println(ledState);
+                } else if (dimmingHigh && ledState < maxLedIntensity) {
+                    ledState += 1;
+                    ledcWrite(PWM1_Ch, ledState);
+                    Serial.print("Dimming High ");
+                    Serial.println(ledState);
+                }
+                delay(20);
+            }
+            dimmingHigh = !dimmingHigh;
+        } else {
+            // Tap actions
+            turnOnLed = ledState == 0;
+            switching = !switching;
+            Serial.println("Switching");
         }
-        delay(20);
-      }
-      dimmingHigh = !dimmingHigh;
-    } else {
-      //Tap actions
-      turnOnLed = ledState == 0;
-      switching = !switching;
-      Serial.println("Switching");
     }
-  }
 
-  unsigned long currentMillis = millis();
+    unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= 10) {
-    previousMillis = currentMillis;
-    iterate = true;
-  }
-
-  if (iterate && switching) {
-    if (ledState < maxLedIntensity && turnOnLed) {
-      dimmingHigh = false;
-      ledState += 1;
-      ledcWrite(PWM1_Ch, ledState);
-    } else if (ledState > 0 && !turnOnLed) {
-      dimmingHigh = true;
-      ledState -= 1;
-      ledcWrite(PWM1_Ch, ledState);
-    } else {
-      switching = false;
+    if (currentMillis - previousMillis >= 10) {
+        previousMillis = currentMillis;
+        iterate = true;
     }
-  }
-  iterate = false;
-  if (ledState > maxLedIntensity) {
-    ledState = maxLedIntensity;
-  }
-  if (ledState < 0) {
-    ledState = 0;
-  }
+
+    if (iterate && switching) {
+        if (ledState < maxLedIntensity && turnOnLed) {
+            dimmingHigh = false;
+            ledState += 1;
+            ledcWrite(PWM1_Ch, ledState);
+        } else if (ledState > 0 && !turnOnLed) {
+            dimmingHigh = true;
+            ledState -= 1;
+            ledcWrite(PWM1_Ch, ledState);
+        } else {
+            switching = false;
+        }
+    }
+    iterate = false;
+    if (ledState > maxLedIntensity) {
+        ledState = maxLedIntensity;
+    }
+    if (ledState < 0) {
+        ledState = 0;
+    }
 }
