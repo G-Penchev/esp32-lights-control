73ebbe636c466451185fdd400c3cefa8ec1e1d56
Georgi Penchev <gpenchev@Georgis-MacBook-Pro.local>
2023-12-18 00:07:56 +0200

    added webpage

--- src/esp-led.ino
+++ src/esp-led.ino
@@ -1,8 +1,16 @@
 #include <Arduino.h>
+#include <ESPmDNS.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ESPAsyncWebServer.h>
+
+#include "webpage.h"
 
 const int numButtons = 1;
 const int buttonPins[numButtons] = {34};
 const int ledPins[numButtons] = {16};
+
 bool buttonState[numButtons] = {false};
 bool lastButtonState[numButtons] = {false};
 unsigned long buttonPressStartTime[numButtons] = {0};
@@ -11,20 +19,91 @@ bool longPressActive[numButtons] = {false};
 bool ledState[numButtons] = {false};
 bool dimmingDirection[numButtons] = {true}; // true for increasing, false for decreasing
 int brightness[numButtons] = {0};
-const int fadeStep = 1; // Adjust the step size for fading
+const int maxBrightness[numButtons] = {900};
+int currentBrightness[numButtons] = {0};
 
-const int freq = 5000;
+const int fadeStep = 1; // Adjust the step size for fading
+const int freq = 500;
 const int resolution = 10;
+
+// SSID and Password of your WiFi router
+const char *ssid = "cocoapop_deco";
+const char *password = "BruteForce_8991";
+
+// Declare a global object variable from the ESP8266WebServer class.
+AsyncWebServer server(80); // Server on port 80
 
 void setup()
 {
     Serial.begin(115200);
+
+    WiFi.begin(ssid, password); // Connect to your WiFi router
+    pinMode(23, OUTPUT);
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        digitalWrite(23, LOW);
+        delay(150);
+        Serial.print(".");
+        digitalWrite(23, HIGH);
+        delay(150);
+    }
+
+    // If connection successful show IP address in serial monitor
+    Serial.println("");
+    digitalWrite(23, HIGH);
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP()); // IP address assigned to your ESP
+
+    // Route setup
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+    String html = MAIN_page;
+    request->send(200, "text/html", html); });
+
+    server.on("/set", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+    if (request->hasParam("light") && request->hasParam("brightness")) {
+      int i = request->getParam("light")->value().toInt();
+      int newBrightness = request->getParam("brightness")->value().toInt();
+      if (i >= 0 && i < numButtons && newBrightness >= 0 && newBrightness <= 800) {
+        brightness[i] = newBrightness;
+        ledState[i] = brightness[i] != 0;
+        ledcWrite(i, getLogValueForTenBit(brightness[i]));
+        request->send(200, "text/plain", "OK");
+      } else {
+        request->send(400, "text/plain", "Invalid parameters");
+      }
+    } else {
+      request->send(400, "text/plain", "Missing parameters");
+    } });
+
+    server.on("/toggle", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+    if (request->hasParam("light")) {
+      int i = request->getParam("light")->value().toInt();
+      
+      if (i >= 0 && i < numButtons) {
+        toggleLight(i);
+        request->send(200, "text/plain", "OK");
+      } else {
+        request->send(400, "text/plain", "Invalid parameters");
+      }
+    } else {
+      request->send(400, "text/plain", "Missing parameters");
+    } });
+
+    server.begin();
+    Serial.println("HTTP server started");
 
     for (int i = 0; i < numButtons; i++)
     {
         pinMode(buttonPins[i], INPUT_PULLUP);
         ledcSetup(i, freq, resolution);
         ledcAttachPin(ledPins[i], i);
+        ledcWrite(i, 0);
     }
 }
 
@@ -59,8 +138,12 @@ void loop()
             // Dimming logic for long press
             if (longPressActive[i])
             {
-                if (dimmingDirection[i] && brightness[i] < 1023)
+                if (dimmingDirection[i] && brightness[i] < maxBrightness[i])
                 {
+                    if (brightness[i] < 300)
+                    {
+                        brightness[i] = 300;
+                    }
                     brightness[i] += fadeStep;
                 }
                 else if (!dimmingDirection[i] && brightness[i] > 0)
@@ -68,7 +151,7 @@ void loop()
                     brightness[i] -= fadeStep;
                 }
                 ledState[i] = brightness[i] != 0;
-                dimLed(brightness[i], i);
+                ledcWrite(i, getLogValueForTenBit(brightness[i]));
             }
         }
         else
@@ -78,10 +161,7 @@ void loop()
                 // Short press
                 Serial.print("Short press detected = ");
                 Serial.println(ledState[i]);
-                ledState[i] = !ledState[i];
-                dimmingDirection[i] = ledState[i];
-                brightness[i] = ledState[i] ? 1023 : 0;
-                dimLed(brightness[i], i);
+                toggleLight(i);
             }
             // Button released
             longPressActive[i] = false;
@@ -92,12 +172,25 @@ void loop()
     }
 }
 
-void dimLed(int value, int channel){
-    Serial.print("Dim set to = ");
+int getLogValueForTenBit(int value)
+{
     Serial.print(value);
-    Serial.print(" and mappedValue set to = ");
+    Serial.print(" = ");
+    int log = int(pow(2, value / 102.3) - 1);
+    Serial.println(log);
+    return log;
+}
 
-    int mappedValue = int(pow(2, value / 102.3) - 1);
-    Serial.println(mappedValue);
-    ledcWrite(channel, mappedValue);
+void toggleLight(int i)
+{
+    ledState[i] = !ledState[i];
+    dimmingDirection[i] = ledState[i];
+    currentBrightness[i] = brightness[i];
+    brightness[i] = ledState[i] ? maxBrightness[i] : 0;
+    while (currentBrightness[i] != brightness[i])
+    {
+        currentBrightness[i] > brightness[i] ? currentBrightness[i]-- : currentBrightness[i]++;
+        ledcWrite(i, getLogValueForTenBit(currentBrightness[i]));
+        delay(1);
+    }
 }
--- src/webpage.h
+++ src/webpage.h
@@ -1,14 +1,159 @@
-const char MAIN_page[] PROGMEM = R"=====(
-<!DOCTYPE html>
-<html>
-<body>
-<center>
-<h1>WiFi LED on off demo: 1</h1><br>
-Ciclk to turn <a href="ledOn">LED ON</a><br>
-Ciclk to turn <a href="ledOff">LED OFF</a><br>
-<hr>
-</center>
+const char MAIN_page[] PROGMEM = R"=====(<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=   device-width, initial-scale=1.0">
+    <title>Lifechanging Lights Control</title>
+    <style>
+        html {
+            min-height: 100%;
+            /* Unlike 'height', which is used by
+                                        @Paulie_D, this will make your webpage
+                                        automatically resize when it exceeds
+                                        the 100% height, thus it won't start the
+                                        gradient over
+                                      */
+        }
+
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 20px;
+            position: fixed;
+            width: 400px;
+            height: 800px;
+            background: linear-gradient(90deg, rgba(71,71,71,1) 11%, rgba(196,195,195,1) 100%);
+        }
+
+        h2 {
+            color: aliceblue;
+        }
+
+        .light-control {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .slider {
+            width: 80%;
+        }
+
+        .button {
+            display: block;
+            width: 15%;
+            padding: 10px;
+            margin: 5px;
+            font-size: 16px;
+        }
+
+        .control-wrap {
+            display: flex;
+            width: 100%;
+        }
+
+        .row-label {
+            width: 100%;
+            padding-top: 3%;
+            padding-right: 40px;
+            color: white;
+
+        }
+
+        input[type=checkbox] {
+            height: 0;
+            width: 0;
+            visibility: hidden;
+        }
+
+        .toggle {
+            cursor: pointer;
+            text-indent: -9999px;
+            width: 60px;
+            height: 30px;
+            background: grey;
+            display: block;
+            border-radius: 100px;
+            position: relative;
+        }
+
+        .toggle:after {
+            content: '';
+            position: absolute;
+            top: 1px;
+            left: 1px;
+            width: 28px;
+            height: 28px;
+            background: #fff;
+            border-radius: 28px;
+            transition: 0.3s;
+        }
+
+        input:checked+label {
+            background: #13c400;
+        }
+
+        input:checked+label:after {
+            left: calc(100% - 1px);
+            transform: translateX(-100%);
+        }
+
+        label:active:after {
+            width: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <h2>Life-changing Lights Control</h2>
+    <div class="light-control">
+        <label class="row-label" for="slider1">Kitchen</label>
+        <div class="control-wrap">
+            <input type="range" min="0" max="1023" value="0" class="slider" id="slider1"
+                oninput="updateBrightness(0, this.value)">
+            <input type="checkbox" id="switch" /><label class="toggle" for="switch">Toggle</label>
+        </div>
+        <label class="row-label" for="slider2">Kitchen Light warmth</label>
+        <div class="control-wrap">
+            <input type="range" min="0" max="1023" value="0" class="slider" id="slider1"
+                oninput="updateBrightness(0, this.value)">
+            <input type="checkbox" id="switch1" /><label class="toggle" for="switch1">Toggle</label>
+        </div>
+        <label class="row-label" for="slider3">Livingroom</label>
+        <div class="control-wrap">
+            <input type="range" min="0" max="1023" value="0" class="slider" id="slider1"
+                oninput="updateBrightness(0, this.value)">
+            <input type="checkbox" id="switch2" /><label class="toggle" for="switch2">Toggle</label>
+        </div>
+        <label class="row-label" for="slider4">Livingroom Light warmth</label>
+        <div class="control-wrap">
+            <input type="range" min="0" max="1023" value="0" class="slider" id="slider1"
+                oninput="updateBrightness(0, this.value)">
+            <input type="checkbox" id="switch3" /><label class="toggle" for="switch3">Toggle</label>
+        </div>
+    </div>
+
+    <script>
+        function updateBrightness(light, value) {
+            document.getElementById(`slider${light + 1}`).value = value;
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/set?light=' + light + '&brightness=' + value, true);
+            xhr.send();
+        }
+
+        function toggleLight(light) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/toggle?light=' + light, true);
+            xhr.send();
+        }
+
+        function updateLightState(value) {
+            document.getElementById("lightState[0]").innerHTML = "Sensor Value: " + value;
+        }
+    </script>
 </body>
+
 </html>
 )=====";
